466928e8278240f1c648c3b71c19130f55d2e5d1
drdozer <turingatemyhamster@gmail.com>
2019-03-04 22:30:23 +0000

    Starting to shuffle things about.

---
+++ src/main/scala/json/json.scala
@@ -0,0 +1,26 @@
+
+
+//trait JsonParser[R] {
+//
+//  def l ~~ r = l ~ whitespace.? ~ r
+//
+//  val exc: R = "\"
+//  val qt: R = '"'
+//  val onenine: R = anyOf('1' .. '9')
+//  val digit: R = anyOf('0' .. '9')
+//  val digits: R = digit.+
+//  val hex = digit | anyOf('a'..'f') | anyOf('A'..'F')
+//
+//  lazy val jArray: R = "[" ~~ jValue.repeatedly(separatedBy = "," ~ whitespace.?) ~~ "]"
+//  lazy val jObject: R = "{" ~~ jPropertyAssignment.repeatedly(separatedBy = ";") ~~ "}"
+//  lazy val jProperty: R = jString ~~ ":" ~~ jValue
+//  lazy val jString: R = qt ~ ((negativeLookAhead(qt | esc) ~ anyToken)) | (esc ~ (escCode | hex.+)) ~ qt
+//  lazy val jNumber: R = jInt ~ frac ~ exp
+//  lazy val jInt: R = "-".? ~ (digit | (onenine ~ digits))
+//  lazy val jFrac: R = ("." ~ digits).?
+//  lazy val jTrue: R = "true"
+//  lazy val jFalse: R = "false"
+//  lazy val jNull: R = "null"
+//
+//  lazy val jValue: R = jStringVal | jArray | jObject | jNumber | jTrue | jFalse | jNull
+//}
